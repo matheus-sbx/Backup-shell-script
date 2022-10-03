2e2590120c86bf9109243570839e767ce8e80065
matheus-sbx <matheus.businessc@gmail.com>
2022-10-03 13:53:19 -0300

    fourth commit

--- backup.sh
+++ backup.sh
@@ -29,17 +29,24 @@
 #           - Inserção do manual de uso                    #
 #         01/10/2022, Matheus                              #
 #           - Adicionando tratamentos de erro              #
+#         03/10/2022, Matheus                              #
+#           - Adicionando diretório Backup                 #
 ############################################################
 
 #..............................VARIÁVEIS..............................#
-
-VERSION="v1.1"
+FILE=
+FILE_CONFIG="config.cf"
+DIR_BACKUP=
+SAVE_DIR_BACKUP=
+CONFIRMATION=0
+VERSION="v1.2"
 COMPACT=
 DIR=
-NAME=
+OLD_FILE=
+NEW_FILE=
 MANUAL="
   $(basename $0) - [OPÇÕES]
-    -n - (opcional) especifica o nome da arquivo que será gerado
+    -n - (opcional) especifica o nome do arquivo que será gerado
     -c - define o compactador que será utilizado [zip/tar]
     -d - especifica o diretório que será compactado
     -h - exibi o manual de uso
@@ -51,15 +58,14 @@ COMPACT_NO_FOUND="\033[31m[ERRO]\033[0m - Compactador Desconhecido."
 SUCCESS="\033[32m[SUCESSO]\033[0m - Compactação realizada com sucesso."
 FAILURE="\033[31m[ERRO]\033[0m - Não foi possível compactar os arquivos."
 
-#..............................TESTES..............................#
-
-while getopts "vhn:c:d:" OPT
+# #..............................TESTES..............................#
+while getopts "vhn:c:d:f" OPT
 do
   case "$OPT" in
     "c") COMPACT="$OPTARG"           ;;
     "d") DIR="$OPTARG"               ;;
     "h") echo "$MANUAL" && exit 0    ;;
-    "n") NAME="$OPTARG"                ;;
+    "n") NEW_FILE="$OPTARG"          ;;
     "v") echo "$VERSION" && exit 0   ;;
     "?") exit 1                      ;;
     *) echo -e "$HELP" && exit 1   ;;
@@ -77,23 +83,57 @@ if [[ $COMPACT != "tar" && $COMPACT != "zip" ]]; then
 fi
 
 # já existe um arquivo com este nome?
-if [[ -e $NAME ]]; then
+if [[ -e $NEW_FILE ]]; then
   read -p "Já existe um arquivo com este nome, deseja subistituir? [y/n]" RESP
-  [[ "$RESP" != "y" && "$RESP" != "Y" ]] && exit 0 || rm $NAME
+  [[ "$RESP" != "y" && "$RESP" != "Y" ]] && exit 0 || rm $NEW_FILE
 fi
 
-[[ $COMPACT = "tar" ]] && expression="backup-$(date +%Y-%m-%d).tar.gz" || expression="backup-$(date +%Y-%m-%d).zip"
-[[ -e $expression ]] && read -p "Já existe um backup realizado nesta data, deseja subistituir? [y/n]" RESP
-[[ "$RESP" != "y" && "$RESP" != "Y" ]] && exit 0 || rm $expression
+[[ $COMPACT = "tar" ]] && OLD_FILE="backup-$(date +%Y-%m-%d).tar.gz" || OLD_FILE="backup-$(date +%Y-%m-%d).zip"
 
-#..............................EXECUÇÃO..............................#
+if [[ -e $OLD_FILE ]]; then
+  read -p "Já existe um arquivo com este nome, deseja subistituir? [y/n]" RESP
+  [[ $RESP = "Y" || $RESP = "y" ]] && rm $OLD_FILE || exit 0
+fi
+
+# #..............................FUNÇÕES..............................#
+
+set_parameters (){
+  local parameter="$(echo $1 | cut -d : -f 1)"
+  local value="$(echo $1 | cut -d : -f 2)"
+
+  case $parameter in
+    "DIR_BACKUP")DIR_BACKUP=$value           ;;
+    "SAVE_DIR_BACKUP")SAVE_DIR_BACKUP=$value ;;
+  esac
+
+}
+# #..............................EXECUÇÃO..............................#
 
 if [[ $COMPACT = "tar" ]]; then
-    [[ -z $NAME ]] && tar -czf backup-$(date +%Y-%m-%d).tar.gz $DIR || tar -czf $NAME $DIR
+    [[ -z $NEW_FILE ]] && FILE=backup-$(date +%Y-%m-%d).tar.gz || FILE=$NEW_FILE
+    [[ -z $NEW_FILE ]] && tar -czf backup-$(date +%Y-%m-%d).tar.gz $DIR || tar -czf $NEW_FILE $DIR
     [[ $? -eq 0 ]] && echo -e "$SUCCESS" || echo -e "$FAILURE"
 fi
 
 if [[ $COMPACT = "zip" ]]; then
-     [[ -z $NAME ]] && zip -rq backup-$(date +%Y-%m-%d).zip $DIR  || zip -r $NAME $DIR
+     [[ -z $NEW_FILE ]] && FILE=backup-$(date +%Y-%m-%d).zip || FILE=$NEW_FILE
+     [[ -z $NEW_FILE ]] && zip -r backup-$(date +%Y-%m-%d).zip $DIR || zip -r $NEW_FILE $DIR
      [[ $? -eq 0 ]] && echo -e "$SUCCESS" || echo -e "$FAILURE"
+fi
+
+while read -r line
+do
+  [[ "$(echo $line | cut -c1)" = "#" ]] && continue
+  [[ ! "$line" ]] && continue
+  set_parameters "$line"
+done < "$FILE_CONFIG"
+
+if [[ "$DIR_BACKUP" = "true" ]]; then
+  find /home/$(whoami)/Backup > /dev/null 2>&1
+  [[ $? -ne 0 ]] && mkdir /home/$(whoami)/Backup
+  CONFIRMATION=1
+fi
+
+if [[ "$SAVE_DIR_BACKUP" = "true" ]]; then
+   [[ $CONFIRMATION -eq 1 ]] && mv "$FILE" /home/$(whoami)/Backup || echo "Diretório Backup/ não encontrado."
 fi
